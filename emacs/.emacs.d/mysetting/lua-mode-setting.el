0c321758c4598ad34d45a4a64890919d3b4e305c
cpper <topcpper@gmail.com>
2014-07-26 11:34:42 +0800

    added lua-mode

---
+++ emacs/.emacs.d/mysetting/lua-mode-setting.el
@@ -0,0 +1,42 @@
+;;; tools.el --- some tool func
+
+;; Copyright (C) 2013  cpper
+
+;; Author: cpper <cpper@cpper.hust.lab>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/lua/")
+
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(lua-indent-level 4))
+
+
+(provide 'lua-mode-setting)
+;;; tools.el ends here
